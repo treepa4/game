ed1d8e3a513f6f82f58ab1f4d6b57112c67c5e2f
h1ro <75143051+treepa4@users.noreply.github.com>
2024-11-25 23:42:15 +0300

    Towers
    Кнопки кликабельные
    Башни можно ставить на плейсы
    небольшие изменения карты

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Loader<TowerManager>
 {
@@ -14,7 +15,30 @@ public class TowerManager : Loader<TowerManager>
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePoint, Vector2.zero);
+
+            
+
+
+            if (hit.collider.tag == "PlaceForTower")
+            {
+                PlaceTower(hit);
+            }
+
+        } 
+    }
+
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && towerBtnPressed != null)
+        {
+            GameObject newTower = Instantiate(towerBtnPressed.TowerObject);
+            newTower.transform.position = hit.transform.position;
+        }    
+
     }
 
     public void SelectedTower(TowersBtn towerSelected)

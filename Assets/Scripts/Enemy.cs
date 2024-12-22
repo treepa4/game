bf77721535dae6bda44bf3bdf9037ed21ecf621e
h1ro <75143051+treepa4@users.noreply.github.com>
2024-12-22 19:11:28 +0300

    люди всегда готовы дать свою любовь кому нибудь кто этого заслуживает
    будь терпимее к людям и знай что быть добрым всегда самый беспроигрышный вариант

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,14 +9,26 @@ public class Enemy : MonoBehaviour
      float speed;
     [SerializeField]
      int nextPoint = 0;
+    [SerializeField]
+    int health;
+    Collider2D enemycollider;
     public float reachThreshold = 0.1f; // Допустимая погрешность для достижения точки
     GameObject exit;
-
+    bool isDead = false;
     // Массив точек движения
     GameObject[] mPoint;
 
-    void Start()
+    public bool IsDead
     {
+        get
+        {
+            return isDead;
+        }
+    }
+
+    void Start()
+    {   
+        enemycollider = GetComponent<Collider2D>();
         // Находим все объекты с тегом MoveingPoint
         mPoint = GameObject.FindGameObjectsWithTag("MoveingPoint");
 
@@ -27,22 +39,36 @@ public class Enemy : MonoBehaviour
         Manager.Instance.RegEnemy(this);
     }
 
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Projectile")) // Проверяем, что это снаряд
+        {
+            Projectile newP = collision.gameObject.GetComponent<Projectile>();
+            Enemyhit(newP.AttackDamage);
+            
+            Destroy(collision.gameObject); // Уничтожаем снаряд
+        }
+    }
+
+
+
      void Update()
     {
         // Проверка, что объект ещё движется по точкам
-        if (nextPoint < mPoint.Length)
+        if (nextPoint < mPoint.Length && isDead == false)
         {
             // Движение к следующей точке
             MoveTowards(mPoint[nextPoint].transform.position);
 
             // Проверяем, достигли ли текущей цели
-            if (Vector2.Distance(transform.position, mPoint[nextPoint].transform.position) < reachThreshold)
+            if (Vector2.Distance(transform.position, mPoint[nextPoint].transform.position) < reachThreshold )
             {
                 nextPoint += 1; // Переключаемся на следующую точку
             }
         }
         // Движение к точке выхода (после последней точки)
-        else if (nextPoint == mPoint.Length)
+        else if (nextPoint == mPoint.Length && isDead == false  )
         {
             MoveTowards(exit.transform.position);
 
@@ -55,10 +81,30 @@ public class Enemy : MonoBehaviour
         }
     }
 
+
     // Метод для движения к заданной позиции
     void MoveTowards(Vector3 targetPosition)
     {
         // Перемещаем объект к цели со скоростью speed
         transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
     }
+
+    public void Enemyhit(int hitPoints)
+    {
+        if (health - hitPoints  > 0)
+        {
+            health -= hitPoints;
+        }
+        else 
+        {
+            Die();
+        }
+
+    }
+    public void Die()
+    {
+        isDead = true;
+        enemycollider.enabled = false;
+    }
+
 }
--- Assets/Scripts/Towers/TowerControl.cs
+++ Assets/Scripts/Towers/TowerControl.cs
@@ -25,7 +25,7 @@ public class TowerControl : MonoBehaviour
     {
         attackCounter -= Time.deltaTime;
         
-        if (targetEnemy == null)
+        if (targetEnemy == null || targetEnemy.IsDead)
         {
             Enemy nearestEnemy = GetNearEnemy();
             // if (nearestEnemy != null)
